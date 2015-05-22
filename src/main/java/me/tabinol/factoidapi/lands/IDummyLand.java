bacc19009ab4c391638abf0ba563e76914ea16f9
Tabinol <michel@localhost>
2015-05-22 16:48:36 -0400

    add copyOfPermsFlags

--- src/main/java/me/tabinol/factoidapi/lands/IDummyLand.java
+++ src/main/java/me/tabinol/factoidapi/lands/IDummyLand.java
@@ -50,6 +50,13 @@ public interface IDummyLand {
     public World getWorld();
     
     /**
+     * Create a new dummyLand with the specific name. Permissions and flags are copied.
+     * @param worldName The name of the dummy land or world
+     * @return the dummy land
+     */
+    public IDummyLand copyOfPermsFlags(String worldName);
+    
+    /**
      * Gets the sets of player containers having a permission.
      *
      * @return the sets the player containers
